a230817677dcdff54cad5a2deb0e2e19823c9686
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-08-11 14:51:14 +0100

    shading!

--- level.gd
+++ level.gd
@@ -10,11 +10,11 @@ func _input(event: InputEvent) -> void:
 	var mouse_click = event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT
 	if mouse_click:
 		started = true
-		ball.velocity.y = -500
+		ball.velocity.y = -400
 		# change velocity angle randomly
 		ball.velocity = ball.velocity.rotated(deg_to_rad(randf_range(-20, 20)))
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if not started:
 		ball.position.x = paddle.position.x
 		ball.position.y = paddle.position.y - 16
